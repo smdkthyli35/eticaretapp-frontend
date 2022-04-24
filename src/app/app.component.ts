6072f81b9b6c12b0132231f4241d59731b92fb82
zeljkosamed <smdkthyli@gmail.com>
2022-04-24 15:53:12 +0300

    Spinner animasyonu componentlere eklendi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any
 
 @Component({
@@ -10,22 +8,6 @@ declare var $: any
 })
 export class AppComponent {
   title = 'client';
-  constructor(private toastrService: CustomToastrService) {
-    toastrService.message('Merhaba!', 'Toastr fun!', {
-      messageType:ToastrMessageType.Info,
-      position: ToastrPosition.TopRight
-    });
-    toastrService.message('Merhaba!', 'Toastr fun!',{
-      messageType:ToastrMessageType.Error,
-      position: ToastrPosition.BottomCenter
-    });
-    toastrService.message('Merhaba!', 'Toastr fun!',{
-      messageType:ToastrMessageType.Success,
-      position: ToastrPosition.BottomLeft
-    });
-    toastrService.message('Merhaba!', 'Toastr fun!',{
-      messageType:ToastrMessageType.Warning,
-      position: ToastrPosition.TopLeft
-    });
+  constructor() {
   } 
 }
